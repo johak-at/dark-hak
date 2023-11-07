a1581d97f1682e8525c0061aa45fd1f877b5a6a9
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-07 19:05:43 +0100

    created routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,13 @@
 <script>
 	import '../app.postcss';
-	let theme = 'cyberpunk';
+	let theme = 'synthwave';
 </script>
 
-<div data-theme={theme} class="min-h-screen p-3">
-	<nav class="mb-3">
-		<a href="/">home</a>
-		<a href="/about">about</a>
-		<a href="/students">students</a>
-	</nav>
-
+<div data-theme={theme} class="min-h-screen flex flex-col items-center justify-center gap-5">
 	<select bind:value={theme} class="fixed top-0 right-0">
+		<option value="synthwave">synthwave</option>
 		<option value="cyberpunk">cyberpunk</option>
 		<option value="coffee">coffee</option>
-		<option value="synthwave">synthwave</option>
 	</select>
 
 	<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,21 @@
 	import Icon from '@iconify/svelte';
 	import { onMount } from 'svelte';
 	let countdown = 60;
+	let stock = 10;
 	onMount(() => {
 		setInterval(() => (countdown = countdown - 1), 1000);
 		setInterval(() => (stock = stock - 1), Math.random() * 10000 + 5000);
 	});
-	let stock = 10;
 </script>
 
 <h1>home</h1>
 <p>this is the home page.</p>
 <button class="btn btn-primary">hallo</button>
 <Icon icon="game-icons:bloody-sword" class="text-red-700 text-7xl" />
-<span class="countdown font-mono text-6xl">
-	<span style="--value:{countdown};" />
-</span>
+<div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content items-center">
+	<span class="countdown font-mono text-5xl">
+		<span style="--value:{countdown};" />
+	</span>
+	sec
+</div>
 <div class="radial-progress text-primary" style="--value:{stock};">{stock}</div>
