4f846a43730f16452b4d41a322669587646577ef
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-13 22:06:07 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,5 @@
 <script>
-	import Icon from '@iconify/svelte';
-	import { onMount } from 'svelte';
 	import { patterns } from '$lib/store';
-
-	let countdown = 60;
-	let stock = 10;
-
-	onMount(() => {
-		setInterval(() => (countdown = countdown - 1), 1000);
-		setInterval(() => (stock = stock - 1), Math.random() * 10000 + 5000);
-	});
 </script>
 
 <ul class="flex flex-col items-center gap-5">
@@ -21,21 +11,6 @@
 		</li>
 	{/each}
 </ul>
-
-<hr class="w-[80%]" />
-
-<button class="btn btn-primary">hallo</button>
-
-<Icon icon="game-icons:bloody-sword" class="text-red-700 text-7xl" />
-
-<div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content items-center">
-	<span class="countdown font-mono text-5xl">
-		<span style="--value:{countdown};" />
-	</span>
-	sec
-</div>
-
-<div class="radial-progress text-primary" style="--value:{stock};">{stock}</div>
 
 <style>
 	.done {
