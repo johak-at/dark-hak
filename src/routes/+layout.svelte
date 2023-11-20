e37a287bed5af70581720dbf17255abb67bc126b
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-20 22:15:35 +0100

    styled main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	let theme = 'synthwave';
 </script>
 
-<div data-theme={theme} class="min-h-screen flex flex-col items-center justify-center gap-5">
+<div data-theme={theme} class="min-h-screen flex flex-col items-center justify-center gap-5 py-32">
 	<select bind:value={theme} class="fixed top-0 right-0">
 		<option value="synthwave">synthwave</option>
 		<option value="cyberpunk">cyberpunk</option>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,26 +6,68 @@
 	}
 </script>
 
-<p class="prose">
-	Als <em>Dark Patterns</em> bzw. <em>Deceptive Design</em> werden insbesondere Online-Designpraktiken
-	bezeichnet, die User*innen zu Handlungen veranlassen, welche sie unbeeinflusst so nicht setzen würden.
-	Sie basieren auf der gezielten Ausnutzung einer Reihe menschlicher Schwächen.
-</p>
+<main class="prose">
+	<h1>dark-hak</h1>
 
-<ul class="flex flex-col items-center gap-5">
-	{#each $patterns as pattern}
+	<p>
+		Als <em>Dark Patterns</em> bzw. <em>Deceptive Design</em> werden insbesondere Online-Designpraktiken
+		bezeichnet, die User*innen zu Handlungen veranlassen, welche sie unbeeinflusst so nicht setzen würden.
+		Sie basieren auf der gezielten Ausnutzung einer Reihe menschlicher Schwächen.
+	</p>
+
+	<p>
+		<em>Dark Patterns</em> erfahren aktuell viel Aufmerksamkeit. Da sie im kurzlebigen Internet immer
+		wieder in neuen Kombinationen auftreten und sie oft im rechtlichen Graubereich liegen, ist ihrer
+		Verbreitung aber nur schwer beizukommen.
+	</p>
+
+	<p>
+		Das wirksamste Mittel zum Schutz vor diesen manipulativen Designpraktiken ist Aufklärung und
+		zahlreiche Initiativen bieten wertvolle Informationen und gesammelte Beispiele, u.a. widmet sich
+		der VKI auf den Seiten
+		<a href="https://konsument.at/dark-patterns-so-manipulieren-websites-und-apps/66873"
+			>konsument.at</a
+		>
+		und <a href="https://europakonsument.at/dark-patterns/66704">europakonsument.at</a> ausführlich der
+		Thematik.
+	</p>
+
+	<p>
+		Basierend auf diesen und weiteren Quellen<sup>[<a href="#quellen">1</a>]</sup> haben
+		Schüler*innen der <a href="https://www.johak.at">JO.HAK</a>
+		auf der vorliegenden Website einen kleinen interaktiven "Hindernisparcours" zur Demonstration einer
+		Reihe verschiedener <em>Dark Patterns</em> erstellt. Wurde eine Aufgabe erfolgreich bewältigt, wird
+		sie von der Liste gestrichen:
+	</p>
+</main>
+
+<nav>
+	<ul class="flex flex-col items-center gap-5 my-5">
+		{#each $patterns as pattern}
+			<li>
+				<a
+					href="/{pattern.slug}"
+					class="transition-all hover:underline {pattern.done ? 'line-through' : ''}"
+					>{pattern.name}</a
+				>
+			</li>
+		{/each}
+	</ul>
+</nav>
+
+<button on:click={clearProgress} class="btn mb-5">Clear Progress</button>
+
+<aside id="quellen" class="prose">
+	<h2>Zusätzliche Quellen</h2>
+	<ul>
 		<li>
-			<a href="/{pattern.slug}" class="transition-all hover:underline {pattern.done ? 'done' : ''}"
-				>{pattern.name}</a
-			>
+			Dark Patterns: Manipulation im Netz - Studie vom ÖIAT im Auftrag der AK Wien. (<a
+				href="https://wien.arbeiterkammer.at/darkpatterns">Link</a
+			>)
 		</li>
-	{/each}
-</ul>
-
-<button on:click={clearProgress} class="btn">Clear Progress</button>
-
-<style>
-	.done {
-		text-decoration: line-through;
-	}
-</style>
+		<li>
+			Brignull, H. (2023): Deceptive patterns - exposing the tricks that tech companies use to
+			control you. (<a href="https://www.deceptive.design/book">Link</a>)
+		</li>
+	</ul>
+</aside>
