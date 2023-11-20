b7971af7ac667cc10fe3a1eeea4978f2f74be67a
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-20 22:56:27 +0100

    added view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
+	import { onNavigate } from '$app/navigation';
 	import '../app.postcss';
+
 	let theme = 'synthwave';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <div data-theme={theme} class="min-h-screen flex flex-col items-center justify-center gap-5 py-32">
