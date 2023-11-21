d3b103469d001f407bb15b4b0822a1b462431d34
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-21 12:21:44 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
 	data-theme={$theme}
 	class="min-h-screen flex flex-col items-center justify-center gap-5 py-32 px-4"
 >
-	<select bind:value={$theme} class="fixed top-0 right-0 select select-bordered">
+	<select bind:value={$theme} class="fixed top-1 right-1 select select-bordered">
 		<option value="synthwave">synthwave</option>
 		<option value="cyberpunk">cyberpunk</option>
 		<option value="johak">johak</option>
