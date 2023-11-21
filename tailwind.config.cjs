61fe74d805ac26ba31dd2d9b2397ef40ae30e9ac
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-21 12:06:29 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,12 +8,14 @@ const config = {
 
 	plugins: [require("daisyui"), require('@tailwindcss/typography')],
 	daisyui: {
-		themes: ["synthwave", "cyberpunk", {
+		themes: ["synthwave", "cyberpunk", "light", {
 			johak: {
+				...require("daisyui/src/theming/themes")["light"],
 				"primary": "#005577",
 				"secondary": "#E4003A",
 				"accent": "#F7A823",
 				"base-100": "#FFFFFF",
+				"neutral": "#1A2E3C"
 			}
 		}]
 	},
