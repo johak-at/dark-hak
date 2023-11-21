ea5db4b559d6bbe09b439367bf9e41f5d0d07781
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2023-11-21 12:15:48 +0100

    added theme to localstorage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script>
 	import { onNavigate } from '$app/navigation';
+	import { theme } from '$lib/store';
 	import '../app.postcss';
-
-	let theme = 'synthwave';
 
 	onNavigate((navigation) => {
 		if (!document.startViewTransition) return;
@@ -17,10 +16,10 @@
 </script>
 
 <div
-	data-theme={theme}
+	data-theme={$theme}
 	class="min-h-screen flex flex-col items-center justify-center gap-5 py-32 px-4"
 >
-	<select bind:value={theme} class="fixed top-0 right-0">
+	<select bind:value={$theme} class="fixed top-0 right-0">
 		<option value="synthwave">synthwave</option>
 		<option value="cyberpunk">cyberpunk</option>
 		<option value="johak">johak</option>
