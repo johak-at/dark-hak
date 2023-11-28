576b695de9e97e2f2e0aef5a17961e3e6a6c891f
Dullinger-g <dullinger.gabriel@johak.at>
2023-11-28 11:49:11 +0100

    delete

--- src/routes/simulated-urgency/+page.svelte
+++ src/routes/simulated-urgency/+page.svelte
@@ -1,8 +1,45 @@
-<<<<<<< HEAD
-<script></script>
-=======
-<script>
-	/** @type {import('./$types').PageData} */
-	export let data;
-</script>
->>>>>>> ff5423214c1d6307059a0514c98f662d16c6d6bf
+<h1>Simulated Urgency</h1>
+<div class="boxes">
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<figure>
+			<p></p>
+			
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Produkt1</h2>
+			<p>produkt1</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">kaufen</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl1">
+		<figure>
+			<p></p>
+			
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Produkt2</h2>
+			<p>produkt2</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">kaufen</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl2">
+		<figure>
+			<p></p>
+			
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Produkt3</h2>
+			<p>produkt3</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">kaufen</button>
+			</div>
+		</div>
+	</div>
+</div>
+
