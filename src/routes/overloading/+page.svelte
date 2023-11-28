80ea3b3052473cb38ee2c516b0cca897c6cd27d7
Zimtaepfelmafia <laemmerhofer.nikolas@gmail.com>
2023-11-28 10:48:37 +0100

    Update +page.svelte

--- src/routes/overloading/+page.svelte
+++ src/routes/overloading/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Migei, Pezi und Lämmerhofer sind die Besten</h1>
-<h2>Niemand ist so toll wie wir</h2>
+<script>
+</script>
+
+<div>
+	<h1>Wähle deine Fremdsprache</h1>
+	<div>
+		<ul>
+			<li><input type="radio" name="radio-8" class="radio radio-error" />FRANZÖSISCH</li>
+			<li><input type="radio" name="radio-9" class="radio" disabled />spanisch</li>
+			<li><input type="radio" name="radio-9" class="radio" disabled />italienisch</li>
+		</ul>
+	</div>
+</div>
