f86d20f62847a922d546aefffca9db0a275f6052
Dullinger-g <dullinger.gabriel@johak.at>
2023-11-28 12:16:20 +0100

    Update +page.svelte

--- src/routes/simulated-urgency/+page.svelte
+++ src/routes/simulated-urgency/+page.svelte
@@ -3,13 +3,12 @@
 	<div class="card w-96 bg-base-100 shadow-xl">
 		<figure>
 			<p></p>
-			
 		</figure>
 		<div class="card-body">
 			<h2 class="card-title">Produkt1</h2>
 			<p>produkt1</p>
 			<div class="card-actions justify-end">
-				<button class="btn btn-primary">kaufen</button>
+				<button class="btn btn-primary">Kaufen</button>
 			</div>
 		</div>
 	</div>
@@ -17,13 +16,12 @@
 	<div class="card w-96 bg-base-100 shadow-xl1">
 		<figure>
 			<p></p>
-			
 		</figure>
 		<div class="card-body">
 			<h2 class="card-title">Produkt2</h2>
 			<p>produkt2</p>
 			<div class="card-actions justify-end">
-				<button class="btn btn-primary">kaufen</button>
+				<button class="btn btn-primary">Kaufen</button>
 			</div>
 		</div>
 	</div>
@@ -31,15 +29,14 @@
 	<div class="card w-96 bg-base-100 shadow-xl2">
 		<figure>
 			<p></p>
-			
+		
 		</figure>
 		<div class="card-body">
 			<h2 class="card-title">Produkt3</h2>
 			<p>produkt3</p>
 			<div class="card-actions justify-end">
-				<button class="btn btn-primary">kaufen</button>
+				<button class="btn btn-primary">Kaufen</button>
 			</div>
 		</div>
 	</div>
 </div>
-
