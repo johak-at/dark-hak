80925a0c105c332c5a6002398bae32f457963951
Zimtaepfelmafia <laemmerhofer.nikolas@gmail.com>
2023-11-28 12:23:43 +0100

    flag

--- src/routes/overloading/+page.svelte
+++ src/routes/overloading/+page.svelte
@@ -2,12 +2,42 @@
 </script>
 
 <div>
-	<h1>Wähle deine Fremdsprache</h1>
-	<div>
+	<img
+		src="https://www.johak.at/wp-content/uploads/2020/08/business-plus-hak-pos.svg"
+		alt="blogo"
+	/>
+	<img src="img/french.svg" alt="" />
+	<h1 class="h-12 text-4xl pb-10 handschrift">Wähle deine Fremdsprache</h1>
+	<div class="bg-cyan-500 rounded-3xl flex w-96 p-10">
 		<ul>
-			<li><input type="radio" name="radio-8" class="radio radio-error" />FRANZÖSISCH</li>
-			<li><input type="radio" name="radio-9" class="radio" disabled />spanisch</li>
-			<li><input type="radio" name="radio-9" class="radio" disabled />italienisch</li>
+			<form>
+				<div class="grid w-96 h-20 rounded bg-red-700 text-primary-content place-content-center">
+					<li class="">
+						<input type="radio" name="radio" class="radio radio-error align-middle" checked /> FRANZÖSISCH
+					</li>
+				</div>
+
+				<div class="grid w-96 h-20 rounded bg-accent text-accent-content place-content-center">
+					<li><input type="radio" name="radio" class="radio align-middle" /> spanisch</li>
+				</div>
+
+				<div
+					class="grid w-96 h-20 rounded bg-secondary text-secondary-content place-content-center"
+				>
+					<li><input type="radio" name="radio" class="radio align-middle" /> italienisch</li>
+				</div>
+			</form>
 		</ul>
 	</div>
 </div>
+
+<div class="mockup-phone border-primary">
+	<div class="camera" />
+
+	<div class="display">
+		<div class="artboard artboard-demo phone-1">Stefano ruft an...</div>
+	</div>
+</div>
+
+<style>
+</style>
