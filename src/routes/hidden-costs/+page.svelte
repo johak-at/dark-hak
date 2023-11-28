be06e20f4f79c84defea08b0c8cc75c3c60fc9c3
Martin Mayrhofer <Mayrhofer.Martin15@gmail.com>
2023-11-28 10:55:02 +0100

    Text

--- src/routes/hidden-costs/+page.svelte
+++ src/routes/hidden-costs/+page.svelte
@@ -2,7 +2,10 @@
 	<div class="mockup-browser-toolbar">
 		<div class="input">https://sicherlichecht.shop</div>
 	</div>
-	<div>Willkommen bei unserem Onlinehsop!</div>
+	<div>Willkommen bei Sicherlich echt!</div>
+	<p>Wir sind ein Unternehmen aus Österreich und verkaufen Süßigkeiten aus der ganzen Welt. 
+		Wir sind zwar erst am Anfang, doch wollen in Zukunft groß hinaus.
+	</p>
 
 	<div class="flex justify-center px-64 py-64 bg-base-800">
 		<button class="btn btn-info">Info</button>
