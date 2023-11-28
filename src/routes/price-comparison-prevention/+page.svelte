9a2a8e4f79dfe5f3d29a97cfb384a295bbac82e6
lesicl05 <84065197+lesicl05@users.noreply.github.com>
2023-11-28 11:03:10 +0100

    .

--- src/routes/price-comparison-prevention/+page.svelte
+++ src/routes/price-comparison-prevention/+page.svelte
@@ -1,1 +1,14 @@
-<h1>lmaoo</h1>
+<h1>Choose your abo</h1>
+
+<div class="card w-96 bg-base-100 shadow-xl">
+	<figure>
+		<img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">Standard</h2>
+		<p>bla bla bla</p>
+		<div class="card-actions justify-end">
+			<button class="btn btn-primary">Choose the abo!</button>
+		</div>
+	</div>
+</div>
