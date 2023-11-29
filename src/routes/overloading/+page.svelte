08152069768ba345d80862898732ee0531d0ce94
rachensp <80815336+rachensp@users.noreply.github.com>
2023-11-29 18:43:12 +0100

    Update +page.svelte

--- src/routes/overloading/+page.svelte
+++ src/routes/overloading/+page.svelte
@@ -1,12 +1,41 @@
 <script>
+	import { onMount } from 'svelte';
+
+	let flagStyle = {
+		position: 'fixed',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: '200px',
+		height: 'auto'
+	};
+
+	onMount(() => {
+		animateFlag();
+	});
+
+	function animateFlag() {
+		setInterval(() => {
+			$: flagStyle = {
+				...flagStyle,
+				top: `${Math.random() * 100}%`,
+				left: `${Math.random() * 100}%`
+			};
+		}, 500);
+	}
 </script>
 
+<div class="navbar bg-gradient-to-r from-blue-500 via-white to-red-500 text-black test" />
 <div>
 	<img
 		src="https://www.johak.at/wp-content/uploads/2020/08/business-plus-hak-pos.svg"
 		alt="blogo"
 	/>
-	<img src="img/french.svg" alt="" />
+	<div
+		style="position: {flagStyle.position}; top: {flagStyle.top}; left: {flagStyle.left}; transform: {flagStyle.transform}; width: {flagStyle.width}; height: {flagStyle.height}"
+	>
+		<img src="img/french.svg" alt="French Flag" />
+	</div>
 	<h1 class="h-12 text-4xl pb-10 handschrift">Wähle deine Fremdsprache</h1>
 	<div class="bg-cyan-500 rounded-3xl flex w-96 p-10">
 		<ul>
