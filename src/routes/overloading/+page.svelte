085cda3fad82afd02a0b6e6b707e2a4a7b37ed30
rachensp <80815336+rachensp@users.noreply.github.com>
2023-11-30 10:30:31 +0100

    Update +page.svelte

--- src/routes/overloading/+page.svelte
+++ src/routes/overloading/+page.svelte
@@ -36,37 +36,70 @@
 	>
 		<img src="img/french.svg" alt="French Flag" />
 	</div>
-	<h1 class="h-12 text-4xl pb-10 handschrift">Wähle deine Fremdsprache</h1>
+	<h1 class="h-12 text-4xl pb-10">Wähle deine Fremdsprache</h1>
 	<div class="bg-cyan-500 rounded-3xl flex w-96 p-10">
 		<ul>
 			<form>
-				<div class="grid w-96 h-20 rounded bg-red-700 text-primary-content place-content-center">
-					<li class="">
+				<div class="grid w-96 h-20 rounded bg-red-500 text-primary-content place-content-center">
+					<li class="text-black text-3xl font-bold">
 						<input type="radio" name="radio" class="radio radio-error align-middle" checked /> FRANZÖSISCH
 					</li>
 				</div>
 
-				<div class="grid w-96 h-20 rounded bg-accent text-accent-content place-content-center">
-					<li><input type="radio" name="radio" class="radio align-middle" /> spanisch</li>
+				<div class="grid w-96 h-20 rounded bg-white text-accent-content place-content-center">
+					<li class="text-gray-300 hover:line-through">
+						<input type="radio" name="radio" class="radio align-middle" /> spanisch
+					</li>
 				</div>
 
-				<div
-					class="grid w-96 h-20 rounded bg-secondary text-secondary-content place-content-center"
-				>
-					<li><input type="radio" name="radio" class="radio align-middle" /> italienisch</li>
+				<div class="grid w-96 h-20 rounded bg-blue-400 text-secondary-content place-content-center">
+					<li class="text-gray-500 italy">
+						<input type="radio" name="radio" class="radio align-middle hover: line-through" />
+						italienisch
+					</li>
 				</div>
 			</form>
 		</ul>
 	</div>
 </div>
 
-<div class="mockup-phone border-primary">
+<div class="mockup-phone border-primary z-50">
 	<div class="camera" />
 
 	<div class="display">
-		<div class="artboard artboard-demo phone-1">Stefano ruft an...</div>
+		<div class="artboard artboard-demo phone-1">
+			<div class="chat chat-start">
+				<div class="chat-image avatar">
+					<div class="w-10 rounded-full">
+						<img alt="Tailwind CSS chat bubble component" src="img/franzose.jpg" />
+					</div>
+				</div>
+				<div class="chat-header">
+					Stephanie aus Frankreich
+					<time class="text-xs opacity-50">12:45</time>
+				</div>
+				<div class="chat-bubble">Französisch ist eine WELTSPRACHE🌏</div>
+				<div class="chat-footer opacity-50" />
+			</div>
+			<div class="chat chat-end">
+				<div class="chat-image avatar">
+					<div class="w-10 rounded-full">
+						<img alt="Tailwind CSS chat bubble component" src="img/nikolas-pb.png" />
+					</div>
+				</div>
+				<div class="chat-header">
+					Nikolus aus Caorle
+					<time class="text-xs opacity-50">12:46</time>
+				</div>
+				<div class="chat-bubble">Ich will auch ein Mann von Welt sein</div>
+				<div class="chat-footer opacity-50">Seen just now</div>
+			</div>
+		</div>
 	</div>
 </div>
 
 <style>
+	.italy:hover::after {
+		content: '🤮🤮🤮';
+	}
 </style>
