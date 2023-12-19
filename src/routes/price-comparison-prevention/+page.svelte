cd0429ed63b9bb1e5e9adaa3090181ece82c8079
lesicl05 <84065197+lesicl05@users.noreply.github.com>
2023-12-19 11:49:15 +0100

    .

--- src/routes/price-comparison-prevention/+page.svelte
+++ src/routes/price-comparison-prevention/+page.svelte
@@ -12,15 +12,14 @@
 {#if page === 1}
 	<h1>Choose your abo</h1>
 	<div class="boxes">
-		<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card w-96 bg-base-100 shadow-xl m-[20px]">
 			<figure>
-				<p>some cool picture</p>
-				<!-- <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /> -->
+				<img src="img/prevention1.jpg" alt="cars" />
 			</figure>
 			<div class="card-body">
 				<h2 class="card-title">BASIC ABO</h2>
 				<h3>5,99€ pro Woche</h3>
-				<p class="small">Steuern und Gebühren nicht inkludiert</p>
+				<!-- <p class="small">Steuern und Gebühren nicht inkludiert</p> -->
 				<ul class="ulstandard">
 					<li>Zugang zur stetig wachsenden Spielebibliothek</li>
 					<li>Jederzeit kündbar</li>
@@ -76,10 +75,9 @@
 			</div>
 		</div>
 
-		<div class="card w-96 bg-base-100 shadow-xl two">
+		<div class="card w-96 bg-base-100 shadow-xl m-[20px]">
 			<figure>
-				<p>some cool picture</p>
-				<!-- <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /> -->
+				<img src="img/prevention2.jpg" alt="cars" />
 			</figure>
 			<div class="card-body">
 				<h2 class="card-title">PLUS ABO</h2>
@@ -145,10 +143,9 @@
 			</div>
 		</div>
 
-		<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card w-96 bg-base-100 shadow-xl m-[20px]">
 			<figure>
-				<p>some cool picture</p>
-				<!-- <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /> -->
+				<img src="img/prevention3.jpg" alt="cars" />
 			</figure>
 			<div class="card-body">
 				<h2 class="card-title">PREMIUM ABO</h2>
@@ -226,17 +223,17 @@
 	.boxes {
 		display: flex;
 		justify-content: space-between;
-		align-items: flex-end;
+		align-items: center;
 		/* or use "space-around" for space around each card */
 	}
 
-	.card {
-		margin-right: 50px;
-	}
+	/* .card {
+		margin-right: 30px;
+	} */
 
-	.small {
+	/* .small {
 		font-size: xx-small;
-	}
+	} */
 
 	.ulstandard li {
 		list-style-type: disc;
